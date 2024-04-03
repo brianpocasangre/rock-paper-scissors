aafbca7cf3153616d3c4d610cc7d4257cbd88c52
brianpocasangre <brianpocasangre1@gmail.com>
2024-04-02 21:45:58 -0700

    add loop to playGame function and prompt for each round

--- main.js
+++ main.js
@@ -11,39 +11,69 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    return 'You Win! Rock beats Scissors';
+    return 'You Win!';
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return 'You Lose! Paper beats Rock';
+    return 'You Lose!';
   } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-    return 'Draw! You both picked Rock';
+    return 'Draw!';
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return 'You Win! Paper beats Rock';
+    return 'You Win!';
   } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-    return 'Draw! You both picked Paper';
+    return 'Draw!';
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    return 'You Lose! Scissors beats Paper';
+    return 'You Lose!';
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    return 'You Lose! Rock beats Scissors';
+    return 'You Lose!';
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return 'You Win! Scissors beats Paper';
+    return 'You Win!';
   } else if (
     playerSelection === 'scissors' &&
     computerSelection === 'scissors'
   ) {
-    return 'Draw! You both picked Scissors';
+    return 'Draw!';
   }
 }
 
+// function playGame() {
+//   let game = 5;
+//   let scoreA;
+//   let scoreB;
+//   playRound(playerSelection, computerSelection);
+//   if (playRound(playerSelection, computerSelection) === NaN) {
+//     scoreA++;
+//   } else if (playRound(playerSelection, computerSelection) === NaN) {
+//     scoreB++;
+//   }
+//   console.log(scoreA);
+//   console.log(scoreB);
+// }
+
 function playGame() {
-  let game = 5;
-  let scoreA;
-  let scoreB;
+  let scoreA = 0;
+  let scoreB = 0;
+  for (i = 0; i < 5; i++) {
+    result = playRound(
+      (playerSelection = prompt(
+        'What do you choose; Rock, Paper, Scissors?'
+      ).toLowerCase()),
+      (computerSelection = getComputerChoice())
+    );
+    switch (result) {
+      case 'You Win!':
+        scoreA += 1;
+        break;
+      case 'You Lose!':
+        scoreB += 1;
+        break;
+      case 'Draw':
+        break;
+    }
+    console.log(result);
+    console.log(playerSelection);
+    console.log(computerSelection);
+    console.log(scoreA);
+    console.log(scoreB);
+  }
 }
 
-const playerSelection = prompt(
-  'What do you choose; Rock, Paper, Scissors?'
-).toLowerCase();
-const computerSelection = getComputerChoice();
-
-// console.log(computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
+console.log(playGame());
